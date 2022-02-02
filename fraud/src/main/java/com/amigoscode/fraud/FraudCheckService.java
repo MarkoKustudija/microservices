db6351b8ede2a18143fb4d7f92ef004fba315bf7
marko <marko@marko.com>
2022-02-02 09:09:42 +0100

    adding fraud module

---
+++ fraud/src/main/java/com/amigoscode/fraud/FraudCheckService.java
@@ -0,0 +1,27 @@
+package com.amigoscode.fraud;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFraudulentCustomer(Integer customerId) {
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .createdAt(LocalDate.now())
+                        .build()
+        );
+        return false;
+    }
+}
